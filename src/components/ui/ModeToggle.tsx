dfe0c4c33ecd67b9c86b694cc96fb58f2841cf51
Sadik Khan Pathan <PathanSadikkhan333@gmail.com>
2025-07-29 01:18:25 +0530

    Add ModeToggle button to switch between light and dark themes

--- src/components/ui/ModeToggle.tsx
+++ src/components/ui/ModeToggle.tsx
@@ -13,7 +13,7 @@ export default function ModeToggle(){
         onClick={() => setTheme(theme === "dark"?"light":"dark")}>
         <SunIcon className="h-[1.2rem] w-[1.2rem]rotate-0 scale-100 transitation-all dark:-rotate-90 dark:scale-0"/>
         <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"/>
-        <span className="sr-only">Toogle theme</span>
+        <span className="sr-only">Toogle themes</span>
         </Button>
     )
 }
