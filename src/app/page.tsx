6267a33309a67ffb471bd7bc1b212e216f3d33b0
Sadik Khan Pathan <PathanSadikkhan333@gmail.com>
2025-07-30 01:31:57 +0530

    Create Home page with simple placeholder content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 
-'use client';
-import {Button} from "@/components/ui/button"
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs';
-export default function Home() {
+export default async function Home() {
+
   return ( 
 
 <div className="m-4">
