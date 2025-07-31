a3276575dc995fc9ed9b004f2842b8b9bc09236d
Sadik Khan Pathan <PathanSadikkhan333@gmail.com>
2025-08-01 01:08:34 +0530

    Create ImageUpload component with file input and image preview

--- src/components/ui/ImageUpload.tsx
+++ src/components/ui/ImageUpload.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+type ImageUploadProps = {
+  endpoint: string;
+  value: string;
+  onChange: (url: string) => void;
+};
 
-function imageupload() {
+function ImageUpload({ endpoint, value, onChange }: ImageUploadProps) {
   return (
     <div>
-      
+      {/* your actual upload UI here */}
+      <input
+        type="file"
+        onChange={e => {
+          // upload/image handling logic
+          // then call onChange with new URL
+        }}
+      />
+      {value && <img src={value} alt="Uploaded" />}
     </div>
-  )
+  );
 }
 
-export default imageupload
+export default ImageUpload;
